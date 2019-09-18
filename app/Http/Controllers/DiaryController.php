764f5902662600647d863e738d918b91e6b246e6
obscure723 <obscure723@yone-lab.com>
2019-09-18 14:51:54 +0800

    no message

---
+++ app/Http/Controllers/DiaryController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Diary;
+use App\Http\Requests\CreateDiary;
+
+class DiaryController extends Controller
+{
+    // 一覧表示の画面を返す
+    public function index()
+    {
+        // diariesテーブルのデータを全件取得
+        // allメソッド：全件データを取得するメソッド
+        $diaries = Diary::all();
+
+        // dd($diaries);   // var_dump + 処理をここで中断
+        // view('フォルダ名.ファイル名(blade.phpは除く)')
+        return view('diaries.index', [
+            'diaries' => $diaries
+        ]);
+    }
+
+    // 新規追加の画面を表示するメソッド
+    public function create()
+    {
+        return view('diaries.create');
+    }
+
+    // 新規追加の画面で投稿ボタンが押された時、
+    // 投稿処理をするメソッド
+    public function store(CreateDiary $request)
+    {
+        // ここからデータの登録処理
+
+        // Diaryモデルのインスタンスを取得
+        $diary = new Diary();
+
+        // 画面で入力されたタイトルを代入
+        $diary->title = $request->title;
+        // 画面で入力された本文を代入
+        $diary->body = $request->body;
+
+        $diary->save(); // DBに保存
+
+        // 一覧ページにリダイレクト
+        // 戻った時のフォームを再送信しますか？の対策のため
+        return redirect()->route('diary.index');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get　GET送信のこと
+// Route::post　POST送信のこと
+
+Route::get('/', 'DiaryController@index')
+    ->name('diary.index');
+
+Route::get('diary/create', 'DiaryController@create')
+    ->name('diary.create');
+
+Route::post('diary/create', 'DiaryController@store')
+    ->name('diary.create');
